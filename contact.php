88b0fe1fb72e5f97781ce812bf3788ea1fbd404b
salsi124 <enzo.massuard@gmail.com>
2026-04-17 10:55:53 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "test_dronemakariaprod@gmail.com"; 
+    $subject = "Nouveau message depuis le site";
+
+    $body = "Nom: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message";
+
+    $headers = "From: $email";
+
+    if(mail($to, $subject, $body, $headers)) {
+        echo "Message envoyé avec succès !";
+    } else {
+        echo "Erreur lors de l'envoi.";
+    }
+}
+?>
